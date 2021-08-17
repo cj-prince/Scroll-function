cbd4929a6fc31d65aba923af665904d07c9283e0
cj-prince <davids.chijioke@gmail.com>
2021-08-17 11:27:23 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// footer date set
+const date = document.getElementById('date')
+
+date.innerHTML = new Date().getFullYear()
+
+// toggle links
+const navToggle = document.querySelector('.nav-toggle')
+const linksContainer = document.querySelector('.links-container') 
+const links = document.querySelector('.links')
+
+navToggle.addEventListener('click', function(){
+    linksContainer.classList.toggle('show-links')
+})
